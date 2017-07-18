1d759e07c9ac7b303c33773994d357f3a7804de7
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-07-18 09:48:45 -0400

    Add basic click interactivity

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,12 @@
 $(function() {
-    $('.cell').click(e=>{
+
+    let turn = 0;
+
+    $('.board').on('click', '.open', e=>{
         let cell = $(e.target);
         cell.addClass('occupied');
+        cell.removeClass('open');
+        cell.text(turn?'X':'O');
+        turn = !turn;
     });
-})
+});
